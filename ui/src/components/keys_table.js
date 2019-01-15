d008d7711762ebc65e32044c94a37b288cab1350
摆码王子 <1146556298@qq.com>
2019-01-15 21:13:36 +0800

    组件化排行榜

---
+++ ui/src/components/keys_table.js
@@ -0,0 +1,54 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import React from "react";
+import ax from "../conf/request";
+import {Table} from "antd";
+
+class KeysTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: [],
+      columns: [{
+        title: 'ranking',
+        dataIndex: 'order',
+        align: 'center'
+      }, {
+        title: 'key',
+        dataIndex: 'name',
+        align: 'center'
+      }, {
+        title: 'times',
+        dataIndex: 'count',
+        align: 'center'
+      }]
+    };
+  }
+
+  componentDidMount() {
+    const _this = this;
+    ax.get('/keys')
+      .then(r => _this.setState({
+        rows: r.data.rows
+      }));
+  }
+
+  render() {
+    return (
+      <Table dataSource={this.state.rows} columns={this.state.columns} pagination={false}/>
+    );
+  }
+}
+
+export default KeysTable
--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -1,44 +1,8 @@
 import styles from './index.css';
-import {Card, Col, Row, Table} from 'antd';
+import {Card, Col, Row} from 'antd';
 import React from "react";
-import ax from '../conf/request'
+import KeysTable from '../components/keys_table'
 
-
-class KeysTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rows: [],
-      columns: [{
-        title: 'order',
-        dataIndex: 'order',
-        align: 'center'
-      }, {
-        title: 'key',
-        dataIndex: 'name',
-        align: 'center'
-      }, {
-        title: 'count',
-        dataIndex: 'count',
-        align: 'center'
-      }]
-    };
-  }
-
-  componentDidMount() {
-    const _this = this;
-    ax.get('/keys')
-      .then(r => _this.setState({
-        rows: r.data.rows
-      }));
-  }
-
-  render() {
-    return (
-      <Table dataSource={this.state.rows} columns={this.state.columns} pagination={false}/>
-    );
-  }
-}
 
 export default function () {
   return (
